512f0d5e439a2dcb111b45e589c566ec351fad9d
fatasoft consulting <fatahouahamadi88@gmail.com>
2024-05-06 10:17:15 +0100

    Add contraint unique vehicleId

---
+++ migrations/20240506090952-add-unique-foreign-key.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addConstraint("VehiculeAnnonces", {
+      fields: ["vehiculeId"],
+      type: "unique",
+      name: "unique_vehicleId_constraint",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeConstraint(
+      "VehiculeAnnonces",
+      "unique_vehicleId_constraint"
+    );
+  },
+};
