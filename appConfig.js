25f249f68935985999c0d3fc7fcb63121b7eaf77
fatasoft consulting <fatahouahamadi88@gmail.com>
2025-01-10 10:38:09 +0100

    Control duplicate content

--- appConfig.js
+++ appConfig.js
@@ -1,13 +1,12 @@
 module.exports = {
   urlClients: {
-    urlSuccess: "https://moovicar-9c0b59206766.herokuapp.com/account",
-    urlRedirect: "https://moovicar-9c0b59206766.herokuapp.com/account",
+    urlSuccess: "https://app.moovicar.com/account",
+    urlRedirect: "https://app.moovicar.com/account",
   },
   google: {
     clientID:
       "93245243307-u7jc6g7kevaumfen935i04vie1g7meg0.apps.googleusercontent.com",
     clientSecret: "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",
-    callbackURL:
-      "https://moovicar-9c0b59206766.herokuapp.com/api/auth/google/callback",
+    callbackURL: "https://app.moovicar.com/api/auth/google/callback",
   },
 };
