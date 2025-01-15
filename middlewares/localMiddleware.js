7e5e16b82940e066580b4e719f41491f28ca9244
fatasoft consulting <fatahouahamadi88@gmail.com>
2025-01-15 10:46:48 +0100

    first commit

--- middlewares/localMiddleware.js
+++ middlewares/localMiddleware.js
@@ -181,10 +181,11 @@ router.post("/login", function (req, res, next) {
       }
       const token = jwt.sign({ sub: req.user.email }, jwtOptions.secretOrKey);
       res.cookie("jwtToken", token, {
-        sameSite: "Lax", // Nécessaire pour permettre les cookies cross-domain
+        sameSite: "Strict", // Nécessaire pour permettre les cookies cross-domain
         httpOnly: false,
         domain: "app.moovicar.com",
         secure: true, // Protège le cookie d'un accès JavaScript
+        maxAge: 24 * 60 * 60 * 1000, // Durée de vie du cookie (1 jour, par exemple)
       });
       return res.status(200).json({ success: true }); // Connexion réussie
     });
