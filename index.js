664468317c970815cd13ca08927897b91701d23e
fatasoft consulting <fatahouahamadi88@gmail.com>
2025-01-15 09:43:40 +0100

    first commit

--- index.js
+++ index.js
@@ -75,10 +75,10 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      sameSite: "Strict", // Nécessaire pour permettre les cookies cross-domain
+      // sameSite: "Strict", // Nécessaire pour permettre les cookies cross-domain
       httpOnly: false,
       domain: ".app.moovicar.com",
-      secure: false, // Protège le cookie d'un accès JavaScript
+      secure: true, // Protège le cookie d'un accès JavaScript
     },
   })
 );
