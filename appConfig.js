050c7f1c54255e52fdf33e2ecbb7a5a84dc85dfe
fatasoft consulting <fatahouahamadi88@gmail.com>
2026-01-17 15:06:11 +0100

    Fix: convert frontend from submodule to regular folder

--- appConfig.js
+++ appConfig.js
@@ -7,6 +7,6 @@ module.exports = {
     clientID:
       "93245243307-u7jc6g7kevaumfen935i04vie1g7meg0.apps.googleusercontent.com",
     clientSecret: "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",
-    callbackURL: "http://localhost:3001/api/auth/google/callback", //"https://app.moovicar.com/api/auth/google/callback",
+    callbackURL: "https://app.moovicar.com/api/auth/google/callback", //"https://app.moovicar.com/api/auth/google/callback",
   },
 };
